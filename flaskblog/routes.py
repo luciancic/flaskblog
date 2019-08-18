13a9f6b6d8f76ffa089c5514aef6f04a1efcf08b
Lucian Condrea <luciancondreadev@gmail.com>
2019-08-18 19:59:00 -0400

    redirect from login and register if logged in

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -20,6 +20,8 @@ def about():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
@@ -37,6 +39,8 @@ def login():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegisterForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
